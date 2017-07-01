8321295f4efd3b81e73338d6ea6c84e9cec4216f
bbaekddo <bbaekddo100@naver.com>
2017-07-01 16:54:31 +0200

    add repeat question

--- src/Honux/Calendar/Calendar.java
+++ src/Honux/Calendar/Calendar.java
@@ -1,12 +1,13 @@
 package Honux.Calendar;
 
 import java.util.Scanner;
+
 public class Calendar {
 
-	private static final int[] maxOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	public static void SampleMonth(){
-	
+	private static final int[] maxOfDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public static void SampleMonth() {
+
 		System.out.println("  일     월      화      수      목      금     토 ");
 		System.out.println(" --------------------------");
 		System.out.println("  1   2   3   4   5   6   7");
@@ -15,22 +16,39 @@ public class Calendar {
 		System.out.println(" 22  23  24  25  26  27  28");
 		System.out.println(" 29  30  31");
 	}
-	
-	public int getDaysOfMonth(int month){
-		
+
+/*	public int whatTime() {
+
+		System.out.println("반복 횟수를 입력하시오.");
+		Scanner whatTime = new Scanner(System.in);
+
+		int a = whatTime.nextInt();
+		return a;
+	}
+*/
+	public int getDaysOfMonth(int month) {
+
 		return maxOfDays[month - 1];
 	}
-	
+
 	public static void main(String[] args) {
 
-		System.out.println("달을 입력하시오.");
-		Scanner whatMonth = new Scanner(System.in);
 		Calendar cal = new Calendar();
-	
-		int a = whatMonth.nextInt();
+
+		//cal.whatTime();
+
+		System.out.println("반복 횟수를 입력하시오.");
+		Scanner whatTime = new Scanner(System.in);
+		int a = whatTime.nextInt();
 		
-		System.out.printf("%d월은 %d월까지 있습니다.", a, cal.getDaysOfMonth(a));
+		System.out.println("달을 입력하시오.");
+		for(int i = 0; i < a; i++) {
+
+			Scanner whatMonth = new Scanner(System.in);
+			int bi = whatMonth.nextInt();
+			System.out.printf("%d월은 %d월까지 있습니다.\n", bi, cal.getDaysOfMonth(bi));
+		}
 
 	}
-
-};;
+    
+}
