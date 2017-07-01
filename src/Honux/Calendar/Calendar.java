a68cc95e29cc99790b88f6e233ef8bdf7c990e42
bbaekddo <bbaekddo100@naver.com>
2017-07-01 15:49:14 +0200

    update

--- src/Honux/Calendar/Calendar.java
+++ src/Honux/Calendar/Calendar.java
@@ -1,9 +1,12 @@
 package Honux.Calendar;
 
+import java.util.Scanner;
 public class Calendar {
 
-	public static void main(String[] args) {
-
+	private static final int[] maxOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public static void SampleMonth(){
+	
 		System.out.println("  일     월      화      수      목      금     토 ");
 		System.out.println(" --------------------------");
 		System.out.println("  1   2   3   4   5   6   7");
@@ -11,7 +14,23 @@ public class Calendar {
 		System.out.println(" 15  16  17  18  19  20  21");
 		System.out.println(" 22  23  24  25  26  27  28");
 		System.out.println(" 29  30  31");
+	}
+	
+	public int getDaysOfMonth(int month){
+		
+		return maxOfDays[month - 1];
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println("달을 입력하시오.");
+		Scanner whatMonth = new Scanner(System.in);
+		Calendar cal = new Calendar();
+	
+		int a = whatMonth.nextInt();
+		
+		System.out.printf("%d월은 %d월까지 있습니다.", a, cal.getDaysOfMonth(a));
 
 	}
 
-}
+};;
